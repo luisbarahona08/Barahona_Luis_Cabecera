4a5d9e2f16e33339ff3a658b8ae121d12ab86b43
Luis Barahona <ldavidbara@gmail.com>
2026-04-28 12:19:24 -0500

    Se agrega funcion de multiplicacion

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "sumar.h"
 #include "restar.h"
+#include "multiplicacion.h"
 #include <stdio.h>
 
 int main(int argc, char const *argv[]) {
@@ -12,5 +13,7 @@ int main(int argc, char const *argv[]) {
     printf ("La suma es: %d\n", resultado);
     int resultado_resta = restar(a, b);
     printf("La resta es: %d\n", resultado_resta);
+    int resultado_multiplicacion = multiplicar(a, b);
+    printf("La multiplicacion es:" "%d\n", resultado_multiplicacion);
     return 0;
 }
